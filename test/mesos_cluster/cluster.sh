00bf9f539441a43f1957005e57a872a0b03aea74
Dmitry Platon <platon.dimka@gmail.com>
2015-12-09 14:08:18 +0300

    Empty slave start/stop added to test environment.

--- test/mesos_cluster/cluster.sh
+++ test/mesos_cluster/cluster.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+#
+# Private functions.
+#
 function script_dir {
     echo "$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 }
 
+#
+# Public functions.
+#
 function start {
-    docker_compose_path=$(script_dir)"/docker-compose.yml"
+    docker_compose_path=$(script_dir)"/cluster.yml"
     docker-compose -f "$docker_compose_path" up -d
 }
 
 function stop {
-    docker_compose_path=$(script_dir)"/docker-compose.yml"
+    docker_compose_path=$(script_dir)"/cluster.yml"
     # We should use kill here for immediately cluster stop.
     docker-compose -f "$docker_compose_path" kill
     docker-compose -f "$docker_compose_path" rm -f
@@ -23,6 +29,21 @@ function restart {
 
 function stop_master {
     docker stop "$1"
+}
+
+function start_empty_slave {
+    docker run --privileged\git 
+               --name=mesos_empty_slave \
+               --link=zk:zk\
+               -d\
+               -e\
+               conf=--master=zk://zk:2181/mesos\
+               mesos_slave
+}
+
+function stop_empty_slave {
+    docker kill mesos_empty_slave
+    docker rm mesos_empty_slave
 }
 
 case "$1" in
@@ -38,7 +59,13 @@ case "$1" in
     stop_master)
         stop_master "$2"
         ;;
+    start_empty_slave)
+        start_empty_slave
+        ;;
+    stop_empty_slave)
+        stop_empty_slave
+        ;;
     *)
-        echo $"Usage: $0 {start|stop|restart|stop_master ID}"
+        echo $"Usage: $0 {start|stop|restart|stop_master ID|start_empty_slave|stop_empty_slave}"
         exit 1
 esac
