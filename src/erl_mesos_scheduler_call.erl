a1b5070ec3a85027fffa6bc85f564b6cf3517fb5
Dmitry Platon <platon.dimka@gmail.com>
2015-12-18 18:27:14 +0300

    base accept call added

--- src/erl_mesos_scheduler_call.erl
+++ src/erl_mesos_scheduler_call.erl
@@ -26,11 +26,19 @@ subscribe(#scheduler_info{request_options = RequestOptions} = SchedulerInfo,
     CallObj = call_obj(SchedulerInfo, Call),
     request(SchedulerInfo1, CallObj).
 
+%% Executes accept call.
+-spec accept(scheduler_info(), call_subscribe()) -> ok | {error, term()}.
 accept(#scheduler_info{subscribed = false}, _CallAccept) ->
     {error, not_subscribed};
-accept(_SchedulerInfo, _CallAccept) ->
-    ok.
-
+accept(#scheduler_info{request_options = RequestOptions} = SchedulerInfo,
+       CallAccept) ->
+    RequestOptions1 = request_options(RequestOptions),
+    SchedulerInfo1 = SchedulerInfo#scheduler_info{request_options =
+                                                  RequestOptions1},
+    CallAcceptObj = call_accept_obj(CallAccept),
+    Call = #call{type = <<"ACCEPT">>, accept = CallAcceptObj},
+    CallObj = call_obj(SchedulerInfo, Call),
+    handle_response(request(SchedulerInfo1, CallObj)).
 
 %% Internal functions.
 
@@ -46,6 +54,12 @@ subscribe_request_options(RequestOptions) ->
     RequestOptions2 = lists:delete(async, RequestOptions1),
     [async, {recv_timeout, infinity}, {following_redirect, false} |
      RequestOptions2].
+
+%% @doc Returns request options.
+%% @private
+-spec request_options(erl_mesos_http:options()) -> erl_mesos_http:options().
+request_options(RequestOptions) ->
+    proplists:delete(async, lists:delete(async, RequestOptions)).
 
 %% @doc Returns call obj.
 %% @private
@@ -65,7 +79,7 @@ call_subscribe_obj(#call_subscribe{framework_info = FrameworkInfo} =
         CallSubscribe#call_subscribe{framework_info = FrameworkInfoObj},
     ?ERL_MESOS_OBJ_FROM_RECORD(call_subscribe, CallSubscribe1).
 
-%% @doc Converts framework info to obj.
+%% @doc Returns framework info obj.
 %% @private
 -spec framework_info_obj(framework_info()) -> erl_mesos_obj:data_obj().
 framework_info_obj(#framework_info{id = FrameworkId,
@@ -88,7 +102,7 @@ framework_id_obj(undefined) ->
 framework_id_obj(FrameworkId) ->
     ?ERL_MESOS_OBJ_FROM_RECORD(framework_id, FrameworkId).
 
-%% @doc Converts framework info capabilitie to obj.
+%% @doc Returns framework info capabilitie obj.
 %% @private
 -spec framework_info_capabilitie_obj(undefined | erl_mesos_obj:data_string()) ->
     undefined | erl_mesos_obj:data_obj().
@@ -98,7 +112,7 @@ framework_info_capabilitie_obj(FrameworkInfoCapabilitie) ->
     ?ERL_MESOS_OBJ_FROM_RECORD(framework_info_capabilitie,
                                FrameworkInfoCapabilitie).
 
-%% @doc Converts labels to obj.
+%% @doc Returns labels obj.
 %% @private
 -spec labels_obj(undefined | labels()) -> erl_mesos_obj:data_obj().
 labels_obj(undefined) ->
@@ -106,6 +120,12 @@ labels_obj(undefined) ->
 labels_obj(#labels{labels = Labels}) ->
     LabelObjs = [?ERL_MESOS_OBJ_FROM_RECORD(label, Label) || Label <- Labels],
     ?ERL_MESOS_OBJ_FROM_RECORD(labels, #labels{labels = LabelObjs}).
+
+%% @doc Returns call accept obj.
+%% @private
+-spec call_accept_obj(call_accept()) -> erl_mesos_obj:data_obj().
+call_accept_obj(#call_accept{} = CallAccept) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(call_accept, CallAccept).
 
 %% @doc Sends http request.
 %% @private
@@ -137,145 +157,23 @@ request_headers(DataFormat) ->
     [{<<"Content-Type">>, erl_mesos_data_format:content_type(DataFormat)},
      {<<"Connection">>, <<"close">>}].
 
-
-
-
-
-
-%% %% @doc Sends subscribe request.
-%% -spec subscribe(erl_mesos_data_format:data_format(), version(), binary(),
-%%                 erl_mesos_http:options(), framework_info()) ->
-%%     {ok, erl_mesos_http:client_ref()} | {error, term()}.
-%% subscribe(DataFormat, Version, MasterHost, Options, FrameworkInfo) ->
-%%     Options1 = subscribe_options(Options),
-%%     FrameworkInfoObj = framework_info_obj_from_record(FrameworkInfo),
-%%     SubscribeObj = erl_mesos_obj:new([{<<"framework_info">>,
-%%                                        FrameworkInfoObj}]),
-%%     ReqObj = erl_mesos_obj:new([{<<"type">>, <<"SUBSCRIBE">>},
-%%                                 {<<"subscribe">>, SubscribeObj}]),
-%%     request(DataFormat, Version, MasterHost, Options1, ReqObj).
-%%
-%% %% @doc Sends subscribe request.
-%% -spec subscribe(erl_mesos_data_format:data_format(), version(), binary(),
-%%                 erl_mesos_http:options(), framework_info(), boolean(),
-%%                 framework_id()) ->
-%%     {ok, erl_mesos_http:client_ref()} | {error, term()}.
-%% subscribe(DataFormat, Version, MasterHost, Options, FrameworkInfo, Force,
-%%           FrameworkId) ->
-%%     Options1 = subscribe_options(Options),
-%%     FrameworkIdObj = ?ERL_MESOS_OBJ_FROM_RECORD(framework_id, FrameworkId),
-%%     FrameworkInfo1 = FrameworkInfo#framework_info{id = FrameworkIdObj},
-%%     FrameworkInfoObj = framework_info_obj_from_record(FrameworkInfo1),
-%%     SubscribeObj = erl_mesos_obj:new([{<<"framework_info">>,
-%%                                        FrameworkInfoObj},
-%%                                       {<<"force">>, Force}]),
-%%     ReqObj = erl_mesos_obj:new([{<<"type">>, <<"SUBSCRIBE">>},
-%%                                 {<<"framework_id">>, FrameworkIdObj},
-%%                                 {<<"subscribe">>, SubscribeObj}]),
-%%     request(DataFormat, Version, MasterHost, Options1, ReqObj).
-%%
-%% %% @doc Sends teardown request.
-%% -spec teardown(erl_mesos_data_format:data_format(), version(), binary(),
-%%                erl_mesos_http:options(), framework_id()) ->
-%%     ok | {error, term()}.
-%% teardown(DataFormat, Version, MasterHost, Options, FrameworkId) ->
-%%     FrameworkIdObj = ?ERL_MESOS_OBJ_FROM_RECORD(framework_id, FrameworkId),
-%%     ReqObj = erl_mesos_obj:new([{<<"type">>, <<"TEARDOWN">>},
-%%                                 {<<"framework_id">>, FrameworkIdObj}]),
-%%     convert_response(request(DataFormat, Version, MasterHost, Options, ReqObj)).
-%%
-%% %% Internal functions.
-%%
-%% %% @doc Returns subscribe options.
-%% %% @private
-%% -spec subscribe_req_options(erl_mesos_http:options()) ->
-%%     erl_mesos_http:options().
-%% subscribe_req_options(ReqOptions) ->
-%%     Keys = [async, recv_timeout, following_redirect],
-%%     ReqOptions1 = [ReqOption || {Key, _Value} = ReqOption <- ReqOptions,
-%%                    not lists:member(Key, Keys)],
-%%     [{async, true},
-%%      {recv_timeout, infinity},
-%%      {following_redirect, false} | ReqOptions1].
-%%
-%% %% @doc Sends http request to the mesos master.
-%% %% @private
-%% -spec request(erl_mesos_data_format:data_format(), version(), binary(),
-%%               erl_mesos_http:options(), erl_mesos_obj:data_obj()) ->
-%%     {ok, erl_mesos_http:client_ref()} |
-%%     {ok, non_neg_integer(), erl_mesos_http:headers(),
-%%      erl_mesos_http:client_ref()} |
-%%     {error, term()}.
-%% request(DataFormat, Version, MasterHost, Options, ReqObj) ->
-%%     Url = request_url(Version, MasterHost),
-%%     ReqHeaders = request_headers(DataFormat),
-%%     ReqBody = erl_mesos_data_format:encode(DataFormat, ReqObj),
-%%     erl_mesos_http:request(post, Url, ReqHeaders, ReqBody, Options).
-%%
-%% %% @doc Returns request url.
-%% %% @private
-%% -spec request_url(version(), binary()) -> binary().
-%% request_url(v1, MasterHost) ->
-%%     <<"http://", MasterHost/binary, ?V1_API_PATH>>.
-%%
-%% %% @doc Returns request headers.
-%% %% @private
-%% -spec request_headers(erl_mesos_data_format:data_format()) ->
-%%     erl_mesos_http:headers().
-%% request_headers(DataFormat) ->
-%%     [{<<"Content-Type">>, erl_mesos_data_format:content_type(DataFormat)},
-%%      {<<"Connection">>, <<"close">>}].
-%%
-%% %% @doc Converts response.
-%% %% @private
-%% -spec convert_response({ok, non_neg_integer(), erl_mesos_http:headers(),
-%%                         reference()} | {error, term()}) ->
-%%     ok | {error, term()} |
-%%     {error, {http_response, non_neg_integer(), binary()}}.
-%% convert_response(Response) ->
-%%     case Response of
-%%         {ok, 202, _Headers, _ClientRef} ->
-%%             ok;
-%%         {ok, Status, _Headers, ClientRef} ->
-%%             case erl_mesos_http:body(ClientRef) of
-%%                 {ok, Body} ->
-%%                     {error, {http_response, Status, Body}};
-%%                 {error, Reason} ->
-%%                     {error, Reason}
-%%             end;
-%%         {error, Reason} ->
-%%             {error, Reason}
-%%     end.
-%%
-%% %% @doc Converts framework info to obj.
-%% %% @private
-%% -spec framework_info_obj_from_record(framework_info()) ->
-%%     erl_mesos_obj:data_obj().
-%% framework_info_obj_from_record(#framework_info{capabilities = Capabilities,
-%%                                                labels = Labels} =
-%%                                FrameworkInfo) ->
-%%     CapabilitiesObj = capabilities_obj(Capabilities),
-%%     LabelsObj = labels_obj_from_record(Labels),
-%%     FrameworkInfo1 = FrameworkInfo#framework_info{capabilities =
-%%                                                       CapabilitiesObj,
-%%                                                   labels = LabelsObj},
-%%     ?ERL_MESOS_OBJ_FROM_RECORD(framework_info, FrameworkInfo1).
-%%
-%% %% @doc Converts capabilities to obj.
-%% %% @private
-%% -spec capabilities_obj(undefined | erl_mesos_obj:data_string()) ->
-%%     undefined | erl_mesos_obj:data_obj().
-%% capabilities_obj(undefined) ->
-%%     undefined;
-%% capabilities_obj(FrameworkInfoCapabilitie) ->
-%%     ?ERL_MESOS_OBJ_FROM_RECORD(framework_info_capabilitie,
-%%                                FrameworkInfoCapabilitie).
-%%
-%% %% @doc Converts labels to obj.
-%% %% @private
-%% -spec labels_obj_from_record(undefined | labels()) -> erl_mesos_obj:data_obj().
-%% labels_obj_from_record(undefined) ->
-%%     undefined;
-%% labels_obj_from_record(#labels{labels = Labels}) ->
-%%     LabelObjs = [?ERL_MESOS_OBJ_FROM_RECORD(label, Label) || Label <- Labels],
-%%     ?ERL_MESOS_OBJ_FROM_RECORD(labels, #labels{labels = LabelObjs}).
+%% @doc Handles response.
+%% @private
+-spec handle_response({ok, non_neg_integer(), erl_mesos_http:headers(),
+                      reference()} | {error, term()}) ->
+    ok | {error, term()} |
+    {error, {http_response, non_neg_integer(), binary()}}.
+handle_response(Response) ->
+    case Response of
+        {ok, 202, _Headers, _ClientRef} ->
+            ok;
+        {ok, Status, _Headers, ClientRef} ->
+            case erl_mesos_http:body(ClientRef) of
+                {ok, Body} ->
+                    {error, {http_response, Status, Body}};
+                {error, Reason} ->
+                    {error, Reason}
+            end;
+        {error, Reason} ->
+            {error, Reason}
+    end.
