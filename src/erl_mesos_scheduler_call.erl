fcc8e6147068349007bc7620173237119b2c742a
Dmitry Platon <platon.dimka@gmail.com>
2015-12-22 15:27:12 +0300

    Resource obj added

--- src/erl_mesos_scheduler_call.erl
+++ src/erl_mesos_scheduler_call.erl
@@ -117,9 +117,11 @@ framework_info_capabilitie_obj(FrameworkInfoCapabilitie) ->
 -spec labels_obj(undefined | labels()) -> erl_mesos_obj:data_obj().
 labels_obj(undefined) ->
     undefined;
-labels_obj(#labels{labels = Labels}) ->
-    LabelObjs = [?ERL_MESOS_OBJ_FROM_RECORD(label, Label) || Label <- Labels],
-    ?ERL_MESOS_OBJ_FROM_RECORD(labels, #labels{labels = LabelObjs}).
+labels_obj(#labels{labels = LabelsList} = Labels) ->
+    LabelObjs = [?ERL_MESOS_OBJ_FROM_RECORD(label, Label) ||
+                 Label <- LabelsList],
+    Labels1 = Labels#labels{labels = LabelObjs},
+    ?ERL_MESOS_OBJ_FROM_RECORD(labels, Labels1).
 
 %% @doc Returns call accept obj.
 %% @private
@@ -166,9 +168,206 @@ offer_operation_obj(#offer_operation{launch = OfferOperationLaunch} =
     undefined | erl_mesos_obj:data_obj().
 offer_operation_launch_obj(undefined) ->
     undefined;
-offer_operation_launch_obj(#offer_operation_launch{task_infos = _TaskInfos} =
+offer_operation_launch_obj(#offer_operation_launch{task_infos = TaskInfos} =
                            OfferOperationLaunch) ->
-    ?ERL_MESOS_OBJ_FROM_RECORD(offer_operation_launch, OfferOperationLaunch).
+    TaskInfoObjs = [task_info_obj(TaskInfo) || TaskInfo <- TaskInfos],
+    OfferOperationLaunch1 =
+        OfferOperationLaunch#offer_operation_launch{task_infos = TaskInfoObjs},
+    ?ERL_MESOS_OBJ_FROM_RECORD(offer_operation_launch, OfferOperationLaunch1).
+
+%% @doc Returns task info obj.
+%% @private
+-spec task_info_obj(task_info()) -> erl_mesos_obj:data_obj().
+task_info_obj(#task_info{task_id = TaskId,
+                         agent_id = AgentId,
+                         resources = Resources} = TaskInfo) ->
+    TaskIdObj = ?ERL_MESOS_OBJ_FROM_RECORD(task_id, TaskId),
+    AgentIdObj = ?ERL_MESOS_OBJ_FROM_RECORD(agent_id, AgentId),
+    ResourceObjs = resource_objs(Resources),
+    TaskInfo1 = TaskInfo#task_info{task_id = TaskIdObj,
+                                   agent_id = AgentIdObj,
+                                   resources = ResourceObjs},
+    ?ERL_MESOS_OBJ_FROM_RECORD(task_info, TaskInfo1).
+
+%% @doc Returns resource objs.
+%% @private
+-spec resource_objs(undefined | [resource()]) ->
+    undefined | [erl_mesos_obj:data_obj()].
+resource_objs(undefined) ->
+    undefined;
+resource_objs(Resources) ->
+    [resource_obj(Resource) || Resource <- Resources].
+
+%% @doc Returns resource obj.
+%% @private
+-spec resource_obj(resource()) -> erl_mesos_obj:data_obj().
+resource_obj(#resource{scalar = ValueScalar,
+                       ranges = ValueRanges,
+                       set = ValueSet,
+                       reservation = ResourceReservationInfo,
+                       disk = ResourceDiskInfo,
+                       revocable = ResourceRevocableInfo} = Resource) ->
+    ValueScalarObj = value_scalar_obj(ValueScalar),
+    ValueRangesObj = value_ranges_obj(ValueRanges),
+    ValueSetObj = value_set_obj(ValueSet),
+    ResourceReservationInfoObj =
+        resource_reservation_info_obj(ResourceReservationInfo),
+    ResourceDiskInfoObj = resource_disk_info_obj(ResourceDiskInfo),
+    ResourceRevocableInfoObj =
+        resource_revocable_info_obj(ResourceRevocableInfo),
+    Resource1 = Resource#resource{scalar = ValueScalarObj,
+                                  ranges = ValueRangesObj,
+                                  set = ValueSetObj,
+                                  reservation = ResourceReservationInfoObj,
+                                  disk = ResourceDiskInfoObj,
+                                  revocable = ResourceRevocableInfoObj},
+    ?ERL_MESOS_OBJ_FROM_RECORD(resource, Resource1).
+
+%% @doc Returns value scalar obj.
+%% @private
+-spec value_scalar_obj(undefined | value_scalar()) ->
+    undefined | erl_mesos_obj:data_obj().
+value_scalar_obj(undefined) ->
+    undefined;
+value_scalar_obj(ValueScalar) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(value_scalar, ValueScalar).
+
+%% @doc Returns value ranges obj.
+%% @private
+-spec value_ranges_obj(undefined | value_ranges()) ->
+    undefined | erl_mesos_obj:data_obj().
+value_ranges_obj(undefined) ->
+    undefined;
+value_ranges_obj(#value_ranges{range = ValueRangesList} = ValueRanges) ->
+    ValueRangeObjs = [?ERL_MESOS_OBJ_FROM_RECORD(value_range, ValueRange) ||
+                      ValueRange <- ValueRangesList],
+    ValueRanges1 = ValueRanges#value_ranges{range = ValueRangeObjs},
+    ?ERL_MESOS_OBJ_FROM_RECORD(value_ranges, ValueRanges1).
+
+%% @doc Returns value set obj.
+%% @private
+-spec value_set_obj(undefined | value_set()) ->
+    undefined | erl_mesos_obj:data_obj().
+value_set_obj(undefined) ->
+    undefined;
+value_set_obj(ValueSet) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(value_set, ValueSet).
+
+%% @doc Returns resource reservation info obj.
+%% @private
+-spec resource_reservation_info_obj(undefined | resource_reservation_info()) ->
+    undefined | erl_mesos_obj:data_obj().
+resource_reservation_info_obj(undefined) ->
+    undefined;
+resource_reservation_info_obj(ResourceReservationInfo) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(resource_reservation_info,
+                               ResourceReservationInfo).
+
+%% @doc Returns resource disk info obj.
+%% @private
+-spec resource_disk_info_obj(undefined | resource_disk_info()) ->
+    undefined | erl_mesos_obj:data_obj().
+resource_disk_info_obj(undefined) ->
+    undefined;
+resource_disk_info_obj(#resource_disk_info{persistence =
+                                               ResourceDiskInfoPersistence,
+                                           volume = Volume} =
+                       ResourceDiskInfo) ->
+    ResourceDiskInfoPersistenceObj =
+        resource_disk_info_persistence_obj(ResourceDiskInfoPersistence),
+    VolumeObj = volume_obj(Volume),
+    ResourceDiskInfo1 =
+        ResourceDiskInfo#resource_disk_info{persistence =
+                                                ResourceDiskInfoPersistenceObj,
+                                            volume = VolumeObj},
+    ?ERL_MESOS_OBJ_FROM_RECORD(resource_disk_info, ResourceDiskInfo1).
+
+%% @doc Returns resource disk info persistence obj.
+%% @private
+-spec resource_disk_info_persistence_obj(undefined |
+                                         resource_disk_info_persistence()) ->
+    undefined | erl_mesos_obj:data_obj().
+resource_disk_info_persistence_obj(undefined) ->
+    undefined;
+resource_disk_info_persistence_obj(ResourceDiskInfoPersistence) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(resource_disk_info_persistence,
+                               ResourceDiskInfoPersistence).
+
+%% @doc Returns volume obj.
+%% @private
+-spec volume_obj(undefined | volume()) -> undefined | erl_mesos_obj:data_obj().
+volume_obj(undefined) ->
+    undefined;
+volume_obj(#volume{image = Image} = Volume) ->
+    ImageObj = image_obj(Image),
+    Volume1 = Volume#volume{image = ImageObj},
+    ?ERL_MESOS_OBJ_FROM_RECORD(volume, Volume1).
+
+%% @doc Returns image obj.
+%% @private
+-spec image_obj(undefined | image()) -> undefined | erl_mesos_obj:data_obj().
+image_obj(undefined) ->
+    undefined;
+image_obj(#image{appc = ImageAppc,
+                 docker = ImageDocker} = Image) ->
+    ImageAppcObj = image_appc_obj(ImageAppc),
+    ImageDockerObj = image_docker_obj(ImageDocker),
+    Image1 = Image#image{appc = ImageAppcObj,
+                         docker = ImageDockerObj},
+    ?ERL_MESOS_OBJ_FROM_RECORD(image, Image1).
+
+%% @doc Returns image appc obj.
+%% @private
+-spec image_appc_obj(undefined | image_appc()) ->
+    undefined | erl_mesos_obj:data_obj().
+image_appc_obj(undefined) ->
+    undefined;
+image_appc_obj(#image_appc{labels = Labels} = ImageAppc) ->
+    LabelsObj = labels_obj(Labels),
+    ImageAppc1 = ImageAppc#image_appc{labels = LabelsObj},
+    ?ERL_MESOS_OBJ_FROM_RECORD(image_appc, ImageAppc1).
+
+%% @doc Returns image docker obj.
+%% @private
+-spec image_docker_obj(undefined | image_docker()) ->
+    undefined | erl_mesos_obj:data_obj().
+image_docker_obj(undefined) ->
+    undefined;
+image_docker_obj(ImageDocker) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(image_docker, ImageDocker).
+
+%% @doc Returns resource revocable info obj.
+%% @private
+-spec resource_revocable_info_obj(undefined | resource_revocable_info()) ->
+    undefined | erl_mesos_obj:data_obj().
+resource_revocable_info_obj(undefined) ->
+    undefined;
+resource_revocable_info_obj(ResourceRevocableInfo) ->
+    ?ERL_MESOS_OBJ_FROM_RECORD(resource_revocable_info, ResourceRevocableInfo).
+
+%% %% @doc Returns discovery info obj.
+%% %% @private
+%% -spec discovery_info_obj(undefined | discovery_info()) ->
+%%     undefined | erl_mesos_obj:data_obj().
+%% discovery_info_obj(undefined) ->
+%%     undefined;
+%% discovery_info_obj(#discovery_info{ports = Ports,
+%%                                    labels = Labels} = DiscoveryInfo) ->
+%%     PortsObj = ports_obj(Ports),
+%%     LabelsObj = labels_obj(Labels),
+%%     DiscoveryInfo1 = DiscoveryInfo#discovery_info{ports = PortsObj,
+%%                                                   labels = LabelsObj},
+%%     ?ERL_MESOS_OBJ_FROM_RECORD(discovery_info, DiscoveryInfo1).
+%%
+%% %% @doc Returns ports obj.
+%% %% @private
+%% -spec ports_obj(undefined | pts()) -> undefined | erl_mesos_obj:data_obj().
+%% ports_obj(undefined) ->
+%%     undefined;
+%% ports_obj(#ports{ports = PortsList} = Ports) ->
+%%     PortObjs = [?ERL_MESOS_OBJ_FROM_RECORD(port, Port) || Port <- PortsList],
+%%     Ports1 = Ports#ports{ports = PortObjs},
+%%     ?ERL_MESOS_OBJ_FROM_RECORD(ports, Ports1).
 
 %% @doc Sends http request.
 %% @private
