d95d7427fa3c3b97be80dd06c82e58aca1cd370c
Dmitry Platon <platon.dimka@gmail.com>
2015-12-23 16:34:54 +0300

    Accept and reconcile examples added

--- test/tmp_scheduler.erl
+++ test/tmp_scheduler.erl
@@ -11,135 +11,118 @@
          resource_offers/3,
          offer_rescinded/3,
          status_update/3,
+         slave_lost/3,
+         executor_lost/3,
          error/3,
          handle_info/3,
          terminate/3]).
+
+-record(state, {offer = accept :: accept | reconcile | decline}).
 
 init(Options) ->
     FrameworkInfo = #framework_info{user = <<"root">>,
                                     name = <<"Erlang test framework">>},
     call_log("== Init callback~n"
              "== Options: ~p~n~n", [Options]),
-    {ok, FrameworkInfo, true, init_state}.
+    Offer = proplists:get_value(offer, Options, accept),
+    {ok, FrameworkInfo, true, #state{offer = Offer}}.
 
-registered(SchedulerInfo, #event_subscribed{} = EventSubscribed, State) ->
+registered(_SchedulerInfo, #event_subscribed{} = EventSubscribed, State) ->
     call_log("== Registered callback~n"
-             "== Scheduler info: ~p~n"
-             "== Event subscribed: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, EventSubscribed, State]),
-    {ok, registered_state}.
+             "== Event subscribed: ~p~n",
+             [EventSubscribed]),
+    {ok, State}.
 
-reregistered(SchedulerInfo, State) ->
-    call_log("== Reregistered callback~n"
-             "== Scheduler info: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, State]),
-    {ok, reregistered_state}.
+reregistered(_SchedulerInfo, State) ->
+    call_log("== Reregistered callback~n", []),
+    {ok, State}.
 
-disconnected(SchedulerInfo, State) ->
-    call_log("== Disconnected callback~n"
-             "== Scheduler info: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, State]),
-    {ok, disconnected_state}.
+disconnected(_SchedulerInfo, State) ->
+    call_log("== Disconnected callback~n", []),
+    {ok, State}.
 
-resource_offers(SchedulerInfo, #event_offers{offers = Offers}, State) ->
-    io:format("Offers ~p~n", [Offers]),
-    [#offer{id = OfferId,
-            agent_id = AgentId} | _] = Offers,
-
-
+resource_offers(SchedulerInfo, #event_offers{offers = Offers} = EventOffers,
+                State) ->
     TaskId = #task_id{value = <<"3">>},
+    State1 =
+    case State of
+        #state{offer = accept} ->
+            [#offer{id = OfferId, agent_id = AgentId} | _] = Offers,
+            CommandValue = <<"while true; do echo 'Test task is running...'; sleep 1; done">>,
+            CommandInfo = #command_info{shell = true,
+                                        value = CommandValue},
+            CpuScalarValue = #value_scalar{value = 0.1},
+            ResourceCpu = #resource{name = <<"cpus">>,
+                                    type = <<"SCALAR">>,
+                                    scalar = CpuScalarValue},
+            TaskInfo = #task_info{name = <<"test_task">>,
+                                  task_id = TaskId,
+                                  agent_id = AgentId,
+                                  command = CommandInfo,
+                                  resources = [ResourceCpu]},
+            Launch = #offer_operation_launch{task_infos = [TaskInfo]},
+            OfferOperation = #offer_operation{type = <<"LAUNCH">>,
+                                              launch = Launch},
+            CallAccept = #call_accept{offer_ids = [OfferId],
+                                      operations = [OfferOperation]},
+            ok = erl_mesos_scheduler:accept(SchedulerInfo, CallAccept),
+            CallReconcileTask = #call_reconcile_task{task_id = TaskId},
+            CallReconcile = #call_reconcile{tasks = [CallReconcileTask]},
+            ok = erl_mesos_scheduler:reconcile(SchedulerInfo, CallReconcile),
+            State#state{offer = decline};
+        #state{offer = decline} ->
+            State
+    end,
+    call_log("== Resource offers callback~n"
+             "== Event offers: ~p~n"
+             "== Offer: ~p~n",
+             [EventOffers, State#state.offer]),
+    {ok, State1}.
 
-%%    CommandInfoUriObj = erl_mesos_obj:new([{<<"value">>, <<"test-executor">>}]),
-%%    CommandInfoObj = erl_mesos_obj:new([{<<"uris">>, [CommandInfoUriObj]},
-%%                                        {<<"shall">>, false}]),
-
-
-
-    CommandValue = <<"while true; do echo 'Test task is running...'; sleep 1; done">>,
-%%    CommandInfoObj = erl_mesos_obj:new([{<<"shall">>, true},
-%%                                        {<<"value">>, CommandValue}]),
-
-    CommandInfo = #command_info{shell = true,
-                                value = CommandValue},
-
-    CpuScalarValue = #value_scalar{value = 0.1},
-
-    %CpuScalarObj = erl_mesos_obj:new([{<<"value">>, 0.1}]),
-
-%%    ResourceCpuObj = erl_mesos_obj:new([{<<"name">>, <<"cpus">>},
-%%                                        {<<"type">>, <<"SCALAR">>},
-%%                                        {<<"scalar">>, CpuScalarObj}]),
-    ResourceCpu = #resource{name = <<"cpus">>,
-                            type = <<"SCALAR">>,
-                            scalar = CpuScalarValue},
-
-%%    TaskInfoObj = erl_mesos_obj:new([{<<"name">>, <<"TEST TASK">>},
-%%                                     {<<"task_id">>, TaskIdObj},
-%%                                     {<<"agent_id">>, AgentIdObj},
-%%                                     {<<"command">>, CommandInfoObj},
-%%                                     {<<"resources">>, [ResourceCpuObj]}]),
-
-    TaskInfo = #task_info{name = <<"test_task">>,
-                          task_id = TaskId,
-                          agent_id = AgentId,
-                          command = CommandInfo,
-                          resources = [ResourceCpu]},
-
-    Launch = #offer_operation_launch{task_infos = [TaskInfo]},
-
-    OfferOperation = #offer_operation{type = <<"LAUNCH">>,
-                                      launch = Launch},
-
-    CallAccept = #call_accept{offer_ids = [OfferId],
-                              operations = [OfferOperation]},
-    Result = erl_mesos_scheduler:accept(SchedulerInfo, CallAccept),
-    io:format("~n~nResult ~p~n~n", [Result]),
-    %erlang:send_after(5000, self(), stop),
-    {ok, State}.
-
-offer_rescinded(SchedulerInfo, #event_rescind{} = EventRescind, State) ->
+offer_rescinded(_SchedulerInfo, #event_rescind{} = EventRescind, State) ->
     call_log("== Offer rescinded callback~n"
-             "== Scheduler info: ~p~n"
-             "== Event rescind: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, EventRescind, State]),
+             "== Event rescind: ~p~n",
+             [EventRescind]),
     {ok, State}.
 
-status_update(SchedulerInfo, #event_update{} = EventUpdate, State) ->
+status_update(_SchedulerInfo, #event_update{} = EventUpdate, State) ->
     call_log("== Update status callback~n"
-             "== Scheduler info: ~p~n"
-             "== Event update: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, EventUpdate, State]),
+             "== Event update: ~p~n",
+             [EventUpdate]),
     {ok, State}.
 
-error(SchedulerInfo, #event_error{} = EventError, State) ->
+slave_lost(_SchedulerInfo, #event_failure{} = EventFailure, State) ->
+    call_log("== Slave lost callback~n"
+             "== Event failure: ~p~n",
+             [EventFailure]),
+    {ok, State}.
+
+executor_lost(_SchedulerInfo, #event_failure{} = EventFailure, State) ->
+    call_log("== Executor lost callback~n"
+             "== Event failure: ~p~n",
+             [EventFailure]),
+    {ok, State}.
+
+error(_SchedulerInfo, #event_error{} = EventError, State) ->
     call_log("== Error callback~n"
-             "== Scheduler info: ~p~n"
-             "== Event error: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, EventError, State]),
+             "== Event error: ~p~n",
+             [EventError]),
     {stop, State}.
 
 handle_info(_SchedulerInfo, stop, State) ->
-    {stop, State};
-handle_info(SchedulerInfo, Info, State) ->
-    call_log("== Info callback~n"
-             "== Scheduler info: ~p~n"
-             "== Info: ~p~n"
-             "== State: ~p~n~n",
-             [SchedulerInfo, Info, State]),
-    {ok, handle_info_state}.
 
-terminate(SchedulerInfo, Reason, State) ->
+    {stop, State};
+handle_info(_SchedulerInfo, Info, State) ->
+    call_log("== Info callback~n"
+             "== Info: ~p~n",
+             [Info]),
+    {ok, State}.
+
+terminate(_SchedulerInfo, Reason, State) ->
     io:format("== Terminate callback~n"
-              "== Scheduler info: ~p~n"
               "== Reason: ~p~n"
               "== State: ~p~n~n",
-              [SchedulerInfo, Reason, State]).
+              [Reason, State]).
 
 call_log(Format, Data) ->
     erl_mesos_logger:info(Format, Data).
