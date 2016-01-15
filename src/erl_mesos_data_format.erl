430825052b200bf131129eab1f978fc66681ff0f
Dmitry <platon.dimka@gmail.com>
2016-01-15 20:19:18 +0300

    Data format changed to protobuf

--- src/erl_mesos_data_format.erl
+++ src/erl_mesos_data_format.erl
@@ -4,51 +4,54 @@
 
 -export([content_type/1]).
 
--export([encode/2, decode/2]).
+-export([encode/2, decode/3]).
 
 -export([decode_events/2]).
 
--type data_format() :: json.
+-type data_format() :: protobuf.
 -export_type([data_format/0]).
+
+-type message() :: tuple().
+-export_type([message/0]).
 
 %% External functions.
 
 %% @doc Returns content type.
--spec content_type(json) -> binary().
-content_type(json) ->
-    <<"application/json">>.
+-spec content_type(data_format()) -> binary().
+content_type(protobuf) ->
+    <<"application/x-protobuf">>.
 
-%% @doc Encodes data.
--spec encode(json, erl_mesos_obj:data()) -> iodata().
-encode(json, Data) ->
-    mochijson2:encode(Data).
+%% @doc Encodes message.
+-spec encode(data_format(), message()) -> iodata().
+encode(protobuf, Message) ->
+    scheduler_protobuf:encode_msg(Message).
 
 %% @doc Decodes binary.
--spec decode(json, binary()) -> erl_mesos_obj:data().
-decode(json, Binary) ->
-    mochijson2:decode(Binary).
+-spec decode(data_format(), binary(), atom()) -> message().
+decode(protobuf, Binary, MessageName) ->
+    scheduler_protobuf:decode_msg(Binary, MessageName).
 
 %% @doc Decodes events.
--spec decode_events(json, binary()) -> [erl_mesos_obj:data()].
-decode_events(json, Binary) ->
-    decode_json_events(Binary, <<>>, []).
+-spec decode_events(data_format(), binary()) -> [message()].
+decode_events(protobuf, Binary) ->
+    decode_protobuf_events(Binary, <<>>, []).
 
 %% Internal functions.
 
 %% @doc Decodes json events.
 %% @private
--spec decode_json_events(binary(), binary(), [erl_mesos_obj:data_obj()]) ->
-    [erl_mesos_obj:data_obj()].
-decode_json_events(<<$\n, Chars/binary>>, SizeChars, Packets) ->
+-spec decode_protobuf_events(binary(), binary(), [message()]) -> [message()].
+decode_protobuf_events(<<$\n, Chars/binary>>, SizeChars, Messages) ->
     Size = binary_to_integer(SizeChars),
     case Chars of
-        <<Packet:Size/binary>> ->
-            lists:reverse([decode(json, Packet) | Packets]);
-        <<Packet:Size/binary, RestChars/binary>> ->
-            decode_json_events(RestChars, <<>>,
-                               [decode(json, Packet) | Packets])
+        <<Message:Size/binary>> ->
+            lists:reverse([decode(protobuf, Message, 'Event') | Messages]);
+        <<Message:Size/binary, RestChars/binary>> ->
+            decode_protobuf_events(RestChars, <<>>,
+                                   [decode(protobuf, Message, 'Event') |
+                                    Messages])
     end;
-decode_json_events(<<Char, Chars/binary>>, SizeChars, Packets) ->
-    decode_json_events(Chars, <<SizeChars/binary, Char>>, Packets);
-decode_json_events(<<>>, _SizeChars, Packets) ->
-    Packets.
+decode_protobuf_events(<<Char, Chars/binary>>, SizeChars, Messages) ->
+    decode_protobuf_events(Chars, <<SizeChars/binary, Char>>, Messages);
+decode_protobuf_events(<<>>, _SizeChars, Messages) ->
+    Messages.
