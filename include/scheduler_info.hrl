5958bf0e7c4b8913e94820925d91061b3cdb91ba
Dmitry <platon.dimka@gmail.com>
2016-01-15 20:31:22 +0300

    Scheduler info added

---
+++ include/scheduler_info.hrl
@@ -0,0 +1,6 @@
+-record(scheduler_info, {data_format :: erl_mesos_data_format:data_format(),
+                         api_version :: erl_mesos_scheduler_call:version(),
+                         master_host :: binary(),
+                         request_options :: erl_mesos_http:options(),
+                         subscribed :: boolean(),
+                         framework_id :: erl_mesos_scheduler:'FrameworkID'()}).
