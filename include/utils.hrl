52f2da3d05c8000c7def317e171164e31ead5631
Dmitry Platon <platon.dimka@gmail.com>
2016-01-21 14:23:47 +0300

    Data types added

--- include/utils.hrl
+++ include/utils.hrl
@@ -1,4 +1,4 @@
--record(resources, {cpus = 0.0,
-                    mem = 0.0,
-                    disk = 0.0,
-                    ports = []}).
+-record(resources, {cpus = 0.0 :: float(),
+                    mem = 0.0 :: float(),
+                    disk = 0.0 :: float(),
+                    ports = [] :: [non_neg_integer()]}).
