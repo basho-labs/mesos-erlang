d12806d3e9bea79f9fe08666bf059da2f073b601
platon <platon.dimka@gmail.com>
2016-03-11 16:29:06 +0200

    Calls queue added

---
+++ src/erl_mesos_calls_queue.erl
@@ -0,0 +1,140 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2016 Basho Technologies Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License. You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied. See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(erl_mesos_calls_queue).
+
+-export([new/0,
+         new/1,
+         len/1,
+         pop_call/1,
+         push_call/2,
+         exec_or_push_call/2,
+         exec_calls/1]).
+
+-record(calls_queue, {calls :: queue(),
+                      max_len :: pos_integer(),
+                      max_num_try_execute :: pos_integer(),
+                      num_try_execute = 0 :: non_neg_integer()}).
+
+-type calls_queue() :: #calls_queue{}.
+-export_type([calls_queue/0]).
+
+-type call() :: {module(), atom(), [term()]}.
+-export_type([call/0]).
+
+-define(DEFAULT_MAX_LEN, 10).
+
+-define(DEFAULT_MAX_NUM_TRY_EXECUTE, 5).
+
+%% External functions.
+
+%% @equiv new()
+-spec new() -> calls_queue().
+new() ->
+    new([]).
+
+%% @doc Creates new queue.
+-spec new([{atom(), term()}]) -> calls_queue().
+new(Options) ->
+    MaxLen = proplists:get_value(max_len, Options, ?DEFAULT_MAX_LEN),
+    MaxNumTryExecute = proplists:get_value(max_num_try_execute, Options,
+                                           ?DEFAULT_MAX_NUM_TRY_EXECUTE),
+    #calls_queue{calls = queue:new(),
+                 max_len = MaxLen,
+                 max_num_try_execute = MaxNumTryExecute}.
+
+-spec len(calls_queue()) -> non_neg_integer().
+len(#calls_queue{calls = Calls}) ->
+    queue:len(Calls).
+
+%% @doc Popes call from the calls queue.
+-spec pop_call(calls_queue()) ->
+    {ok, call(), calls_queue()} | {error, calls_queue_empty}.
+pop_call(#calls_queue{calls = Calls} = CallsQueue) ->
+    case queue:len(Calls) > 0 of
+        true ->
+            {{value, Call}, Calls1} = queue:out(Calls),
+            {ok, Call, CallsQueue#calls_queue{calls = Calls1}};
+        false ->
+            {error, calls_queue_empty}
+    end.
+
+%% @doc Pushes call to the calls queue.
+-spec push_call(call(), calls_queue()) ->
+    {ok, calls_queue()} | {error, calls_queue_len_limit}.
+push_call(Call, #calls_queue{calls = Calls,
+                             max_len = MaxLen} = CallsQueue) ->
+    case queue:len(Calls) < MaxLen of
+        true ->
+            {ok, CallsQueue#calls_queue{calls = queue:in(Call, Calls)}};
+        false ->
+            {error, calls_queue_len_limit}
+    end.
+
+%% @doc Executes call or put call to the calls queue.
+-spec exec_or_push_call(call(), calls_queue()) ->
+    {ok, queue()} | {exec_error, term(), calls_queue()} |
+    {error, calls_queue_len_limit}.
+exec_or_push_call(Call, CallsQueue) ->
+    case execute_call(Call) of
+        ok ->
+            {ok, CallsQueue};
+        {error, Reason} ->
+            case push_call(Call, CallsQueue) of
+                {ok, CallsQueue1} ->
+                    {exec_error, Reason, CallsQueue1};
+                {error, Reason1} ->
+                    {error, Reason1}
+            end
+    end.
+
+-spec exec_calls(calls_queue()) ->
+    {ok, calls_queue()} | {exec_error, term(), calls_queue()} | {error, term()}.
+exec_calls(#calls_queue{max_num_try_execute = MaxNumTryExecute,
+                        num_try_execute = NumTryExecute})
+  when MaxNumTryExecute + 1 =:= NumTryExecute ->
+    {error, calls_queue_try_execute_limit};
+exec_calls(#calls_queue{calls = Calls,
+                        num_try_execute = NumTryExecute} = CallsQueue) ->
+    case queue:len(Calls) > 0 of
+        true ->
+            Call = queue:get(Calls),
+            case execute_call(Call) of
+                ok ->
+                    Calls1 = queue:drop(Calls),
+                    CallsQueue1 = CallsQueue#calls_queue{calls = Calls1,
+                                                         num_try_execute = 0},
+                    exec_calls(CallsQueue1);
+                {error, Reason} ->
+                    CallsQueue1 = CallsQueue#calls_queue{num_try_execute =
+                                                         NumTryExecute + 1},
+                    {exec_error, Reason, CallsQueue1}
+            end;
+        false ->
+            {error, calls_queue_empty}
+    end.
+
+%% Internal functions.
+
+%% @doc Executes call.
+%% @private
+-spec execute_call(call()) -> ok | {error, term()}.
+execute_call({Module, Function, Args}) ->
+    erlang:apply(Module, Function, Args).
---
+++ test/erl_mesos_calls_queue_SUITE.erl
@@ -0,0 +1,127 @@
+%% -------------------------------------------------------------------
+%%
+%% Copyright (c) 2016 Basho Technologies Inc. All Rights Reserved.
+%%
+%% This file is provided to you under the Apache License,
+%% Version 2.0 (the "License"); you may not use this file
+%% except in compliance with the License. You may obtain
+%% a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing,
+%% software distributed under the License is distributed on an
+%% "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+%% KIND, either express or implied. See the License for the
+%% specific language governing permissions and limitations
+%% under the License.
+%%
+%% -------------------------------------------------------------------
+
+-module(erl_mesos_calls_queue_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+
+-export([all/0]).
+
+-export([ok_call_fun/0, error_call_fun/1]).
+
+-export([pop_push_call/1, exec_or_push_call/1, exec_calls/1]).
+
+all() ->
+    [pop_push_call, exec_or_push_call, exec_calls].
+
+ok_call_fun() ->
+    ok.
+
+error_call_fun(Reason) ->
+    {error, Reason}.
+
+%% Test functions.
+
+pop_push_call(_Config) ->
+    %% Push.
+    MaxLen = 100,
+    CallsQueue = erl_mesos_calls_queue:new([{max_len, MaxLen}]),
+    0 = erl_mesos_calls_queue:len(CallsQueue),
+    Numbers = lists:seq(1, MaxLen),
+    PushFun = fun(Num, Queue) ->
+                  {ok, Queue1} =
+                      erl_mesos_calls_queue:push_call(error_call(Num), Queue),
+                  Queue1
+              end,
+    CallsQueue1 = lists:foldl(PushFun, CallsQueue, Numbers),
+    MaxLen = erl_mesos_calls_queue:len(CallsQueue1),
+    {error, calls_queue_len_limit} =
+        erl_mesos_calls_queue:push_call(error_call(MaxLen + 1), CallsQueue1),
+    %% Pop.
+    PopFun = fun(Num, Queue) ->
+                 {ok, {?MODULE, error_call_fun, [{bad_call, Num}]}, Queue1} =
+                      erl_mesos_calls_queue:pop_call(Queue),
+                 Queue1
+             end,
+    CallsQueue2 = lists:foldl(PopFun, CallsQueue1, Numbers),
+    0 = erl_mesos_calls_queue:len(CallsQueue2),
+    {error, calls_queue_empty} = erl_mesos_calls_queue:pop_call(CallsQueue2).
+
+exec_or_push_call(_Config) ->
+    %% Execute.
+    MaxLen = 10,
+    CallsQueue = erl_mesos_calls_queue:new([{max_len, MaxLen}]),
+    0 = erl_mesos_calls_queue:len(CallsQueue),
+    {ok, CallsQueue} =
+        erl_mesos_calls_queue:exec_or_push_call(ok_call(), CallsQueue),
+    %% Push.
+    PushFun = fun(Num, Queue) ->
+                  {exec_error, {bad_call, Num}, Queue1} =
+                      erl_mesos_calls_queue:exec_or_push_call(error_call(Num),
+                                                              Queue),
+                  Queue1
+              end,
+    CallsQueue1 = lists:foldl(PushFun, CallsQueue, lists:seq(1, MaxLen)),
+    MaxLen = erl_mesos_calls_queue:len(CallsQueue1),
+    {error, calls_queue_len_limit} =
+        erl_mesos_calls_queue:exec_or_push_call(error_call(MaxLen + 1),
+                                                CallsQueue1).
+
+exec_calls(_Config) ->
+    MaxLen = 5,
+    MaxNumTryExecute = 3,
+    Options = [{max_len, MaxLen}, {max_num_try_execute, MaxNumTryExecute}],
+    CallsQueue = erl_mesos_calls_queue:new(Options),
+    {error, calls_queue_empty} = erl_mesos_calls_queue:exec_calls(CallsQueue),
+    %% Push.
+    Numbers = lists:seq(1, MaxLen),
+    [_ | Numbers1] = Numbers,
+    {ok, CallsQueue1} = erl_mesos_calls_queue:push_call(ok_call(), CallsQueue),
+    PushFun = fun(Num, Queue) ->
+                  {ok, Queue1} =
+                      erl_mesos_calls_queue:push_call(error_call(Num), Queue),
+                  Queue1
+              end,
+    CallsQueue2 = lists:foldl(PushFun, CallsQueue1, Numbers1),
+    %% Exec ok.
+    MaxLen = erl_mesos_calls_queue:len(CallsQueue2),
+    [Num | _] = Numbers1,
+    {exec_error, {bad_call, Num}, CallsQueue3} =
+        erl_mesos_calls_queue:exec_calls(CallsQueue2),
+    ExecLen = MaxLen - 1,
+    ExecLen = erl_mesos_calls_queue:len(CallsQueue3),
+    %% Exec error.
+    ExecFun = fun(_, Queue) ->
+                  {exec_error, {bad_call, _}, Queue1} =
+                      erl_mesos_calls_queue:exec_calls(Queue),
+                  Queue1
+              end,
+    CallsQueue4 = lists:foldl(ExecFun, CallsQueue3,
+                              lists:seq(1, MaxNumTryExecute)),
+    {error, calls_queue_try_execute_limit} =
+        erl_mesos_calls_queue:exec_calls(CallsQueue4).
+
+%% Internal functions.
+
+ok_call() ->
+    {?MODULE, ok_call_fun, []}.
+
+error_call(Num) ->
+    {?MODULE, error_call_fun, [{bad_call, Num}]}.
