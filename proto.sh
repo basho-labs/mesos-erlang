06c25c4ccca122935dcc210b2ba693d53a90d70a
Dmitry Platon <platon.dimka@gmail.com>
2016-05-13 15:21:48 +0300

    Add wget option

--- proto.sh
+++ proto.sh
@@ -6,8 +6,8 @@ URL_PREFIX=https://raw.githubusercontent.com/apache/mesos
 
 # Download proto files
 mkdir -p ${PARENT}/proto/mesos/v1/scheduler
-wget -q ${URL_PREFIX}/${VSN}/include/mesos/v1/mesos.proto -P ${PARENT}/proto/mesos/v1
-wget -q ${URL_PREFIX}/${VSN}/include/mesos/v1/scheduler/scheduler.proto -P ${PARENT}/proto/mesos/v1/scheduler
+wget -q -N ${URL_PREFIX}/${VSN}/include/mesos/v1/mesos.proto -P ${PARENT}/proto/mesos/v1
+wget -q -N ${URL_PREFIX}/${VSN}/include/mesos/v1/scheduler/scheduler.proto -P ${PARENT}/proto/mesos/v1/scheduler
 
 SCHEDULER=${PARENT}/proto/mesos/v1/scheduler/scheduler.proto
 
