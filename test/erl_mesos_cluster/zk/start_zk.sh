fed22aef5d0c7cc06674b5b75b25704553b56c26
Dmitry Platon <platon.dimka@gmail.com>
2016-05-18 11:52:52 +0300

    Add vars for pathes

--- test/erl_mesos_cluster/zk/start_zk.sh
+++ test/erl_mesos_cluster/zk/start_zk.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-sed -e "s/tickTime=[0-9]*/tickTime=${ZK_TICK_TIME}/" /etc/zookeeper/conf/zoo.cfg > zoo.cfg.tmp && mv zoo.cfg.tmp /etc/zookeeper/conf/zoo.cfg
-sed -e "s/initLimit=[0-9]*/initLimit=${ZK_INIT_LIMIT}/" /etc/zookeeper/conf/zoo.cfg > zoo.cfg.tmp && mv zoo.cfg.tmp /etc/zookeeper/conf/zoo.cfg
-sed -e "s/syncLimit=[0-9]*/syncLimit=${ZK_SYNC_LIMIT}/" /etc/zookeeper/conf/zoo.cfg > zoo.cfg.tmp && mv zoo.cfg.tmp /etc/zookeeper/conf/zoo.cfg
-sed -e "s/clientPort=[0-9]*/clientPort=${ZK_CLIENT_PORT}/" /etc/zookeeper/conf/zoo.cfg > zoo.cfg.tmp && mv zoo.cfg.tmp /etc/zookeeper/conf/zoo.cfg
+CFG_PATH=/etc/zookeeper/conf/zoo.cfg
+SERVER_PATH=/usr/share/zookeeper/bin/zkServer.sh
 
-/usr/share/zookeeper/bin/zkServer.sh start-foreground
+sed -e "s/tickTime=[0-9]*/tickTime=${ZK_TICK_TIME}/" ${CFG_PATH} > ${CFG_PATH}.tmp && mv ${CFG_PATH}.tmp ${CFG_PATH}
+sed -e "s/initLimit=[0-9]*/initLimit=${ZK_INIT_LIMIT}/" ${CFG_PATH} > ${CFG_PATH}.tmp && mv ${CFG_PATH}.tmp ${CFG_PATH}
+sed -e "s/syncLimit=[0-9]*/syncLimit=${ZK_SYNC_LIMIT}/" ${CFG_PATH} > ${CFG_PATH}.tmp && mv ${CFG_PATH}.tmp ${CFG_PATH}
+sed -e "s/clientPort=[0-9]*/clientPort=${ZK_CLIENT_PORT}/" ${CFG_PATH} > ${CFG_PATH}.tmp && mv ${CFG_PATH}.tmp ${CFG_PATH}
+
+${SERVER_PATH} start-foreground
