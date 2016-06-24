7b5a44251949f301de743b1afdd9f71c82c3063b
Nikolay Khabarov <2xl@mail.ru>
2016-06-24 21:26:08 +0300

    new test cases for executor

--- test/erl_mesos_executor_SUITE.erl
+++ test/erl_mesos_executor_SUITE.erl
@@ -35,7 +35,8 @@
          init_per_testcase/2,
          end_per_testcase/2]).
 
--export([registered/1, reregistered/1]).
+-export([registered/1, reregistered/1, launch_task/1, kill_task/1,
+         acknowledged/1, framework_message/1, shutdown/1, error/1]).
 
 -define(LOG, false).
 
@@ -43,7 +44,8 @@ all() ->
     [{group, mesos_cluster, [sequence]}].
 
 groups() ->
-    [{mesos_cluster, [registered, reregistered]}].
+    [{mesos_cluster, [registered, reregistered, launch_task, kill_task,
+                      acknowledged, framework_message, shutdown, error]}].
 
 init_per_suite(Config) ->
     ok = erl_mesos:start(),
@@ -90,21 +92,7 @@ end_per_testcase(TestCase, Config) ->
 
 registered(Config) ->
     log("Registered test cases.", Config),
-    Ref = {erl_mesos_executor, registered},
-    Scheduler = ?config(scheduler, Config),
-    SchedulerOptions = ?config(scheduler_options, Config),
-    SchedulerOptions1 = set_test_pid(SchedulerOptions),
-    Options = ?config(options, Config),
-    {ok, _} = start_scheduler(Ref, Scheduler, SchedulerOptions1, Options,
-                              Config),
-    {registered, {SchedulerPid, _, _}} = recv_reply(registered),
-    {resource_offers, {SchedulerPid, _, EventOffers}} =
-        recv_reply(resource_offers),
-    [Offer | _] = offers(EventOffers),
-    #'Offer'{id = OfferId, agent_id = AgentId} = Offer,
-    TaskId = timestamp_task_id(),
-    SchedulerPid ! {accept, OfferId, AgentId, TaskId},
-    {accept, ok} = recv_reply(accept),
+    start_and_accept_scheduler(Config, registered),
     {registered, {ExecutorInfo, EventSubscribed}} =
         recv_framework_message_reply(registered),
     %% Test executor info.
@@ -114,21 +102,8 @@ registered(Config) ->
 
 reregistered(Config) ->
     log("Reregistered test cases.", Config),
-    Ref = {erl_mesos_executor, reregistered},
-    Scheduler = ?config(scheduler, Config),
-    SchedulerOptions = ?config(scheduler_options, Config),
-    SchedulerOptions1 = set_test_pid(SchedulerOptions),
-    Options = ?config(options, Config),
-    {ok, _} = start_scheduler(Ref, Scheduler, SchedulerOptions1, Options,
-        Config),
-    {registered, {SchedulerPid, _, _}} = recv_reply(registered),
-    {resource_offers, {SchedulerPid, _, EventOffers}} =
-        recv_reply(resource_offers),
-    [Offer | _] = offers(EventOffers),
-    #'Offer'{id = OfferId, agent_id = AgentId} = Offer,
-    TaskId = timestamp_task_id(),
-    SchedulerPid ! {accept, OfferId, AgentId, TaskId},
-    {accept, ok} = recv_reply(accept),
+    {SchedulerPid, AgentId, TaskId} =
+        start_and_accept_scheduler(Config, reregistered),
     {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
         recv_reply(status_update),
     ExecutorId = #'ExecutorID'{value = TaskId#'TaskID'.value},
@@ -142,6 +117,88 @@ reregistered(Config) ->
     #executor_info{subscribed = false} = DisconnectedExecutorInfo,
     %% Test reregister executor info.
     #executor_info{subscribed = false} = ReregisterExecutorInfo.
+
+launch_task(Config) ->
+    log("Launch task test cases.", Config),
+    {SchedulerPid, _AgentId, _TaskId} =
+        start_and_accept_scheduler(Config, launch_task),
+    {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+        recv_reply(status_update),
+    {launch_task, {Update, ExecutorInfo}} =
+        recv_framework_message_reply(launch_task),
+    %% Test update.
+    ok = Update,
+    %% Test executor info.
+    #executor_info{subscribed = true} = ExecutorInfo.
+
+kill_task(Config) ->
+    log("Kill task test cases.", Config),
+    {SchedulerPid, _AgentId, TaskId} =
+        start_and_accept_scheduler(Config, kill_task),
+    {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+        recv_reply(status_update),
+    SchedulerPid ! {kill, TaskId},
+    {kill_task, {ExecutorInfo, EventKill}} =
+        recv_framework_message_reply(kill_task),
+    %% Test executor info.
+    #executor_info{subscribed = true} = ExecutorInfo,
+    %% Test event kill.
+    #'Event.Kill'{task_id = TaskId} = EventKill.
+
+acknowledged(Config) ->
+    log("Acknowledged test cases.", Config),
+    {SchedulerPid, _AgentId, TaskId} =
+        start_and_accept_scheduler(Config, acknowledged),
+    {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+        recv_reply(status_update),
+    {acknowledged, {ExecutorInfo, EventAcknowledged}} =
+        recv_framework_message_reply(acknowledged),
+    %% Test executor info.
+    #executor_info{subscribed = true} = ExecutorInfo,
+    %% Test event acknowledged.
+    #'Event.Acknowledged'{task_id = TaskId} = EventAcknowledged.
+
+framework_message(Config) ->
+    log("Message test cases.", Config),
+    {SchedulerPid, AgentId, TaskId} =
+        start_and_accept_scheduler(Config, shutdown),
+    {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+        recv_reply(status_update),
+    ExecutorId = #'ExecutorID'{value = TaskId#'TaskID'.value},
+    Data = <<"testdata">>,
+    SchedulerPid ! {message, AgentId, ExecutorId, Data},
+    {framework_message, {ExecutorInfo, EventMessage}} = recv_framework_message_reply(framework_message),
+    %% Test executor info.
+    #executor_info{executor_id = ExecutorId} = ExecutorInfo,
+    %% Test event message
+    #'Event.Message'{data = Data} = EventMessage.
+
+shutdown(Config) ->
+    log("Shutdown test cases", Config),
+    {SchedulerPid, AgentId, TaskId} =
+        start_and_accept_scheduler(Config, shutdown),
+    {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+        recv_reply(status_update),
+    ExecutorId = #'ExecutorID'{value = TaskId#'TaskID'.value},
+    SchedulerPid ! {shutdown, ExecutorId, AgentId},
+    {shutdown, ExecutorInfo} = recv_framework_message_reply(shutdown),
+    %% Test executor info.
+    #executor_info{executor_id = ExecutorId} = ExecutorInfo.
+
+error(Config) ->
+    log("Error test cases.", Config),
+    %% TODO TODO TODO
+%%     {SchedulerPid, AgentId, _TaskId} =
+%%         start_and_accept_scheduler(Config, error),
+%%     {status_update, {SchedulerPid, _SchedulerInfo, _EventUpdate}} =
+%%         recv_reply(status_update),
+%%     %% put some wrong value
+%%     ExecutorId = #'ExecutorID'{value = <<"nonexistingid">>},
+%%     SchedulerPid ! {shutdown, ExecutorId, AgentId},
+%%     {error, {EventError, _State}} = recv_framework_message_reply(error),
+%%     %% Test event error.
+%%     true = is_record(EventError, 'Event.Error').
+    ok.
 
 %% Internal functions.
 
@@ -158,6 +215,24 @@ stop_mesos_cluster(Config) ->
 start_scheduler(Ref, Scheduler, SchedulerOptions, Options, Config) ->
     log("Start scheduler. Ref: ~p, Scheduler: ~p.", [Ref, Scheduler], Config),
     erl_mesos:start_scheduler(Ref, Scheduler, SchedulerOptions, Options).
+
+start_and_accept_scheduler(Config, Task) ->
+    Ref = {erl_mesos_executor, Task},
+    Scheduler = ?config(scheduler, Config),
+    SchedulerOptions = ?config(scheduler_options, Config),
+    SchedulerOptions1 = set_test_pid(SchedulerOptions),
+    Options = ?config(options, Config),
+    {ok, _} = start_scheduler(Ref, Scheduler, SchedulerOptions1, Options,
+        Config),
+    {registered, {SchedulerPid, _, _}} = recv_reply(registered),
+    {resource_offers, {SchedulerPid, _, EventOffers}} =
+        recv_reply(resource_offers),
+    [Offer | _] = offers(EventOffers),
+    #'Offer'{id = OfferId, agent_id = AgentId} = Offer,
+    TaskId = timestamp_task_id(),
+    SchedulerPid ! {accept, OfferId, AgentId, TaskId},
+    {accept, ok} = recv_reply(accept),
+    {SchedulerPid, AgentId, TaskId}.
 
 set_test_pid(SchedulerOptions) ->
     [{test_pid, self()} | SchedulerOptions].
