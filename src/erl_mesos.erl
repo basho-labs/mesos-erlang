20a2f1f9b0f4cd310a1058ec96c57cdff58f657d
Dmitry Platon <platon.dimka@gmail.com>
2016-11-16 14:11:16 +0300

    Add env funs

--- src/erl_mesos.erl
+++ src/erl_mesos.erl
@@ -22,7 +22,10 @@
 
 -include("erl_mesos_scheduler_proto.hrl").
 
--export([start_scheduler/4, stop_scheduler/1, stop_scheduler/2]).
+-export([get_env_value/1,
+         get_env_value/2,
+         get_env_converted_value/2,
+         get_env_converted_value/3]).
 
 -type 'AgentID'() :: #'AgentID'{}.
 -export_type(['AgentID'/0]).
@@ -95,20 +98,110 @@
 
 %% External functions.
 
-%% @equiv erl_mesos_scheduler:start_link(Name, Scheduler, SchedulerOptions,
-%%                                       Options)
--spec start_scheduler(atom(), module(), term(),
-                      erl_mesos_scheduler:options()) ->
-    {ok, pid()} | {error, term()}.
-start_scheduler(Name, Scheduler, SchedulerOptions, Options) ->
-    erl_mesos_scheduler:start_link(Name, Scheduler, SchedulerOptions, Options).
+%% @equiv get_env_value(Name, undefined)
+-spec get_env_value(atom()) -> term().
+get_env_value(Name) ->
+    get_env_value(Name, undefined).
 
-%% @equiv stop_scheduler(Name, infinity)
--spec stop_scheduler(term())  -> ok | {error, term()}.
-stop_scheduler(Name) ->
-    erl_mesos_scheduler:stop(Name, infinity).
+%% @doc Returns env value.
+-spec get_env_value(atom(), term()) -> term().
+get_env_value(Name, DefaultValue) ->
+    case os:getenv("MESOS_" ++ string:to_upper(atom_to_list(Name))) of
+        false ->
+            DefaultValue;
+        Value ->
+            Value
+    end.
 
-%% @equiv erl_mesos_scheduler:stop(Name, Timeout)
--spec stop_scheduler(term(), timeout())  -> ok.
-stop_scheduler(Name, Timeout) ->
-    erl_mesos_scheduler:stop(Name, Timeout).
+%% @equiv get_env_converted_value(Type, Name, undefined)
+-spec get_env_converted_value(atom(), atom()) -> term().
+get_env_converted_value(Type, Name) ->
+    get_env_converted_value(Type, Name, undefined).
+
+%% @doc Returns env converted value.
+-spec get_env_converted_value(atom(), atom(), term()) -> term().
+get_env_converted_value(atom, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            list_to_atom(Value)
+    end;
+get_env_converted_value(integer, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            list_to_integer(Value)
+    end;
+get_env_converted_value(float, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            list_to_float(Value)
+    end;
+get_env_converted_value(string, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            Value
+    end;
+get_env_converted_value(binary, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            list_to_binary(Value)
+    end;
+get_env_converted_value(interval, Name, DefaultValue) ->
+    case get_env_value(Name) of
+        undefined ->
+            DefaultValue;
+        Value ->
+            convert_interval(Value)
+    end.
+
+%% Internal functions.
+
+%% @doc Convert interval to milli seconds.
+%% @private
+-spec convert_interval(string()) -> float().
+convert_interval(IntervalValue) ->
+    convert_interval(IntervalValue, [], false).
+
+%% @doc Convert interval to milli seconds.
+%% @private
+-spec convert_interval(string(), string(), boolean()) -> float().
+convert_interval([$. = Char | Chars], IntervalChars, false) ->
+    convert_interval(Chars, [Char | IntervalChars], true);
+convert_interval([Char | Chars], IntervalChars, Dot)
+    when Char >= $0, Char =< $9 ->
+    convert_interval(Chars, [Char | IntervalChars], Dot);
+convert_interval(UnitChars, IntervalChars, true) ->
+    IntervalValue = list_to_float(lists:reverse(IntervalChars)),
+    convert_interval(UnitChars, IntervalValue);
+convert_interval(UnitChars, IntervalChars, false) ->
+    IntervalValue = float(list_to_integer(lists:reverse(IntervalChars))),
+    convert_interval(UnitChars, IntervalValue).
+
+%% @doc Convert interval with unit to milli seconds.
+%% @private
+-spec convert_interval(string(), float()) -> float().
+convert_interval("ns", IntervalValue) ->
+    IntervalValue / 1000000;
+convert_interval("us", IntervalValue) ->
+    IntervalValue / 1000;
+convert_interval("ms", IntervalValue) ->
+    IntervalValue;
+convert_interval("secs", IntervalValue) ->
+    IntervalValue * 1000;
+convert_interval("mins", IntervalValue) ->
+    IntervalValue * 60000;
+convert_interval("hrs", IntervalValue) ->
+    IntervalValue * 3600000;
+convert_interval("days", IntervalValue) ->
+    IntervalValue * 86400000;
+convert_interval("weeks", IntervalValue) ->
+    IntervalValue * 604800000.
